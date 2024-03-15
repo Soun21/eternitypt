a2240acb6f90e18f4a5375d02c4aa277ed4203e6
Soun <113449078+Soun21@users.noreply.github.com>
2024-03-15 11:53:45 +0100

    Add files via upload

--- eternitycplusplus/eternity/headers/PuzzleDisplayer.hpp
+++ eternitycplusplus/eternity/headers/PuzzleDisplayer.hpp
@@ -8,15 +8,17 @@
 
 class PuzzleDisplayer{
     private:
+        // Puzzle
         Puzzle * p;
+
+        // Liste de couleurs aléatoires
         std::vector<std::tuple<int, int, int>> colors;
         
     public:
         //Constructeurs
         PuzzleDisplayer(Puzzle * p);
-        void display();
-        void drawTriangles(sf::RenderWindow& window, sf::Vector2f center, float size,int x,int y);
-        void drawSquare(sf::RenderWindow& window, sf::Vector2f position, float size);
-        void drawGrid(sf::RenderWindow& window);
-
+        void display();// Affiche le puzzle
+        void drawTriangles(sf::RenderWindow& window, sf::Vector2f center, float size,int x,int y);// Dessine les triangles (motifs) de chaque pièces
+        void drawSquare(sf::RenderWindow& window, sf::Vector2f position, float size);// Dessine les carrés (un par pièce)
+        void drawGrid(sf::RenderWindow& window);// Dessine la grille
 };
